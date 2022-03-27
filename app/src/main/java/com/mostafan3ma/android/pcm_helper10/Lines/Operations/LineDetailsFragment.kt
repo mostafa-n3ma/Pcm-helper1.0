a4e60b92d47fdc2ec5e83dfefa12c07c61924478
mostafa-n3ma <77761759+mostafa-n3ma@users.noreply.github.com>
2022-03-27 07:49:19 +0300

    feat:setting up PointsAdapter.kt
    adding the adapter to the Details fragment and applying it to the recycler

--- app/src/main/java/com/mostafan3ma/android/pcm_helper10/Lines/Operations/LineDetailsFragment.kt
+++ app/src/main/java/com/mostafan3ma/android/pcm_helper10/Lines/Operations/LineDetailsFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.mostafan3ma.android.pcm_helper10.Lines.Operations.Points.PointListener
+import com.mostafan3ma.android.pcm_helper10.Lines.Operations.Points.PointsAdapter
 import com.mostafan3ma.android.pcm_helper10.R
 import com.mostafan3ma.android.pcm_helper10.databinding.FragmentLineDetailsBinding
 class LineDetailsFragment : Fragment() {
+    private lateinit var pointsAdapter:PointsAdapter
 
 
     override fun onCreateView(
@@ -17,8 +21,17 @@ class LineDetailsFragment : Fragment() {
     ): View? {
 
         val binding = FragmentLineDetailsBinding.inflate(inflater)
-
         var args=LineDetailsFragmentArgs.fromBundle(requireArguments())
+
+        pointsAdapter= PointsAdapter((PointListener {
+            Toast.makeText(requireContext(),"${it.db}",Toast.LENGTH_SHORT).show()
+        }))
+        pointsAdapter.submitList(args.selectedLine.points)
+        binding.pointsRecycler.adapter=pointsAdapter
+
+
+
+
 
         binding.selectedLine=args.selectedLine
 
--- app/src/main/java/com/mostafan3ma/android/pcm_helper10/Lines/Operations/Points/PointsAdapter.kt
+++ app/src/main/java/com/mostafan3ma/android/pcm_helper10/Lines/Operations/Points/PointsAdapter.kt
@@ -1,4 +1,60 @@
 package com.mostafan3ma.android.pcm_helper10.Lines.Operations.Points
 
-class PointsAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.mostafan3ma.android.pcm_helper10.data.source.database.DamagePoint
+import com.mostafan3ma.android.pcm_helper10.databinding.PointItemBinding
+
+class PointsAdapter(private val clickListener:PointListener)
+    :ListAdapter<DamagePoint,PointViewHolder>(PointDiffCallBack())
+{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PointViewHolder {
+       return PointViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: PointViewHolder, position: Int) {
+        val pointItem=getItem(position)
+        holder.itemView.setOnClickListener {
+            clickListener.onClick(pointItem)
+        }
+        holder.bind(pointItem)
+    }
+
+
+}
+
+
+
+
+class PointViewHolder(private val binding:PointItemBinding):RecyclerView.ViewHolder(binding.root){
+    fun bind(pointItem:DamagePoint){
+        binding.point=pointItem
+        binding.executePendingBindings()
+    }
+    companion object{
+        fun from(parent:ViewGroup):PointViewHolder{
+            val layoutInflater=LayoutInflater.from(parent.context)
+            val binding=PointItemBinding.inflate(layoutInflater,parent,false)
+            return PointViewHolder(binding)
+        }
+    }
+}
+
+class PointDiffCallBack : DiffUtil.ItemCallback<DamagePoint>() {
+    override fun areItemsTheSame(oldItem: DamagePoint, newItem: DamagePoint): Boolean {
+        return oldItem.db == newItem.db
+    }
+
+    override fun areContentsTheSame(oldItem: DamagePoint, newItem: DamagePoint): Boolean {
+    return oldItem==newItem
+    }
+
+}
+
+
+class PointListener(val clickListener: (point: DamagePoint) -> Unit) {
+    fun onClick(point: DamagePoint) = clickListener(point)
 }
