2b6f10593dfdeb2754f07b880ce1de8f154eb73f
mostafa-n3ma <77761759+mostafa-n3ma@users.noreply.github.com>
2022-04-16 22:36:38 +0300

    fix:fixing the up button
    since I was facing a problem with the up button in LineDetailsFragment.kt when I add the optionMenu to it it's breaks the upButton behavior
    some thing I didn't now way exactly but it turns out that  the abbBarConfiguration in the MainActivity was wrong
    finally I found the right solution for it

--- app/src/main/java/com/mostafan3ma/android/pcm_helper10/MainActivity.kt
+++ app/src/main/java/com/mostafan3ma/android/pcm_helper10/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.ui.setupWithNavController
 import com.mostafan3ma.android.pcm_helper10.data.source.*
 import com.mostafan3ma.android.pcm_helper10.databinding.ActivityMainBinding
 import kotlinx.coroutines.MainScope
@@ -27,30 +28,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        val navController=findNavController(R.id.nav_host_fragment_content_main)
+        navController.let {
+            appBarConfiguration= AppBarConfiguration(navController.graph)
+            setSupportActionBar(binding.toolbar)
+            binding.toolbar.setupWithNavController(it,appBarConfiguration)
         }
+
+
     }
+
+
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
